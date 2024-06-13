b2b10a28b09b6e91f25d52ef88c035aa5fa7d9ba
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:25:55 +0100

    exercise_9.py

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,6 @@
-# Exercise 9
-# Your solution comes here
+h =  int(input())
+min = int(input())
+sec = int(input())
+tsec = (h * 3600) + (min * 60) + sec
+H_angle = (tsec / 3600)*360
+print(H_angle)
