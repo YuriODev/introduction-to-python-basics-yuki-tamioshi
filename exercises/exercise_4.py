1815705ea0b1ecf1dd5d50e45e382b93812a5de7
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:00:13 +0100

    exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,9 @@
-# Exercise 4
-# Your solution comes here
+n = int(input('enter a 4 digit no.'))
+d1 = n // 1000 
+d2 = (n % 1000) // 100
+d3 = (n % 100) // 10
+d4 = n % 10 
+if d1 == d4 and d2 == d3:
+  print('1')
+else:
+  print('0')
